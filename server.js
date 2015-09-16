13ae95d355ed4c5dbb3ad115f3a0074e9d6f1858
unknown <mmontano@SDIL-041B.SDI>
2015-09-16 09:31:28 -0400

    updating query

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ app.set('port', process.env.PORT || 5000);
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-app.post('/new', function(req, res) {
+app.post('/update', function(req, res) {
     pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
         conn.query(
-            'INSERT INTO salesforce.IT_Software_Type INTO (softwareName,date,subscription,numberOfLicenses) VALUES(softwareName,date,subcription,number)',
+            'UPDATE salesforce.IT_Software_Type SET number = $1,  WHERE LOWER(softwareName) = LOWER($2) ',
             [req.body.softwareName, req.body.number],
             function(err, result) {
                 done();
