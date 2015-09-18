1e7f578e44f8fac5cb449fe9ddd16b424fae977a
unknown <mmontano@SDIL-041B.SDI>
2015-09-18 16:06:29 -0400

    changing query

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.post('/update', function(req, res) {
 app.post('/new', function(req, res) {
     pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
         
-        var insert = 'INSERT INTO salesforce.Contact(Name) VALUES($1)';
+        var insert = 'INSERT INTO salesforce.Contact(LastName) VALUES($1)';
             conn.query(insert,[req.body.LastName],
             function(err, result) {
                 done();
