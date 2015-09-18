bd6d95443b32b67e5dba25be9d90806b9961627a
unknown <mmontano@SDIL-041B.SDI>
2015-09-18 11:19:17 -0400

    lastName added

--- server.js
+++ server.js
@@ -49,8 +49,8 @@ app.post('/update', function(req, res) {
 app.post('/new', function(req, res) {
     pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
         
-        var insert = 'INSERT INTO salesforce.Contact(Name) VALUES($1)';
-            conn.query(insert,[req.body.Name],
+        var insert = 'INSERT INTO salesforce.Contact(Name,LastName) VALUES($1,$1)';
+            conn.query(insert,[req.body.Name, req.body.LastName],
             function(err, result) {
                 done();
                 if (err != null || result.rowCount == 0) {
