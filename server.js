a74e4664c29617fe3de9f45f95462cdaf22558f8
unknown <mmontano@SDIL-041B.SDI>
2015-09-18 09:55:51 -0400

    query2

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.post('/update', function(req, res) {
                 
                 if (err != null || result.rowCount == 0) {
                     
-                    conn.query('INSERT INTO salesforce.IT_Software_Type__c (number,softwareName , date, subscription) VALUES ($1, $2, $3, $4)',[req.body.number__c, req.body.softwareName, req.body.date, req.body.subscription],
+                    conn.query('INSERT INTO salesforce.IT_Software_Type__c (number__c,softwareName ,date__c, subscription__c) VALUES ($1, $2, $3, $4)',[req.body.number__c, req.body.softwareName, req.body.date, req.body.subscription],
     
                   function(err, result) {
                     done();
