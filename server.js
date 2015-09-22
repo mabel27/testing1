c3653490c6979f028439207a59487374f5914afb
unknown <mmontano@SDIL-041B.SDI>
2015-09-22 12:00:07 -0400

    query Select from salesforce

--- server.js
+++ server.js
@@ -10,6 +10,26 @@ app.set('port', process.env.PORT || 5000);
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
+    
+    pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
+     
+     if (err) console.error(err);
+        
+        var select = 'SELECT Name FROM salesforce.IT_Software_Type__c ';
+        console.log(select);
+        
+      conn.query(select, function(err, result) {
+         
+         if (err){
+        console.log('Insert error: ' + err);
+    }
+
+         });
+          });
+
+
+
+
 app.post('/update', function(req, res) {
     pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
         
