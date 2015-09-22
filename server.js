4cf4ac4e9284214ceb11b391f5b35e620030821c
unknown <mmontano@SDIL-041B.SDI>
2015-09-22 13:11:14 -0400

    dropdown query

--- server.js
+++ server.js
@@ -10,8 +10,9 @@ app.set('port', process.env.PORT || 5000);
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
+app.get('/softwareName',function(req,res) {
     
-    pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
+     pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
      
      if (err) console.error(err);
         
@@ -19,21 +20,23 @@ app.use(bodyParser.json());
         console.log(select);
         
       conn.query(select, function(err, result) {
-         
-        if (err) { 
-      console.error(err);
-    } else {
-      var names = {};
-      result.rows.forEach(function(row){
-        names[row.Name] = row;
-      });
-      console.log('names :', names);
-    }
-         });
-          });
+          
+           if (err != null || result.rowCount != 0) {
+               
+            done();
+            res.json(result);
+           }
+               else {
+                   
+                   res.status(400).json({error: err.message});
+                   
+               }
+        
+            });
+        });
+});
 
-
-
+   
 
 app.post('/update', function(req, res) {
     pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
