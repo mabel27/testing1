2fc486ab79ea81d8debc509cd7ad2cb35cda83fe
unknown <mmontano@SDIL-041B.SDI>
2015-09-25 10:16:52 -0400

    adding external Id

--- server.js
+++ server.js
@@ -82,9 +82,7 @@ app.post('/update', function(req, res) {
             
         );
         
-        
-        
-          var insert = 'INSERT INTO salesforce.IT_Software__c (sfid,number__c) VALUES ($1, $2)';
+          var insert = 'INSERT INTO salesforce.IT_Software__c (ExternalId__c,number__c) VALUES ($1, $2)';
                     
             conn.query(insert,[req.body.sfid, req.body.number__c],function(err, result) {
                 done();
