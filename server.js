40d053da336b71e1e8da8294e671932069718a26
unknown <mmontano@SDIL-041B.SDI>
2015-09-25 11:25:59 -0400

    debugging

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.post('/update', function(req, res) {
         
           var insert = 'INSERT INTO salesforce.IT_Software__c (Id__r__ExternalId__c,number__c) VALUES ($1,$2)';
                     
-            conn.query(insert,[req.body.number__c],function(err, result) {
+            conn.query(insert,[req.body.Id__r__ExternalId__c, req.body.number__c],function(err, result) {
                 done();
                 if (err != null || result.rowCount == 0) {
                      console.error(err);
