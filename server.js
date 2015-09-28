0e40e46e50396e179c4f6ac0d0d1f6a11985dd4a
unknown <mmontano@SDIL-041B.SDI>
2015-09-28 10:16:41 -0400

    debugging

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.post('/update', function(req, res) {
             
         );
         
-          var insert = 'INSERT INTO salesforce.IT_Software__c (IT_Software_Type__c__ExternalId__c,number__c) VALUES ($1)';
+          var insert = 'INSERT INTO salesforce.IT_Software__c (number__c) VALUES ($1)';
                     
             conn.query(insert,[req.body.number__c],function(err, result) {
                 done();
