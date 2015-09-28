6f84c112b3aae1299293607e292c26d9d4b09022
unknown <mmontano@SDIL-041B.SDI>
2015-09-28 16:40:19 -0400

    testing query

--- server.js
+++ server.js
@@ -81,9 +81,12 @@ app.post('/update', function(req, res) {
         */
         
         //IT_Software_Type__c__ExternalId__c
-          var insert = 'INSERT INTO salesforce.IT_Software__c (number__c) VALUES ($1)';
+        
+        
+        
+          var insert = 'INSERT INTO salesforce.IT_Software__c (id__c, sfid ,number__c) VALUES ($3,$3,$1)';
                     
-            conn.query(insert,[req.body.number__c],function(err, result) {
+            conn.query(insert,[req.body.id__c,req.body.sfid,req.body.number__c],function(err, result) {
                 done();
                 if (err != null || result.rowCount == 0) {
                     console.error(err);
