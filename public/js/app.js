261ec3ebfadfdd881680ac06d9cf3205c842e00f
unknown <mmontano@SDIL-041B.SDI>
2015-09-29 14:53:40 -0400

    debugging

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@ var apiVersion = 'v30.0',
 clientId = '3MVG9KI2HHAq33RyxiQvWKh1_dXf93uPDkDWUMddH0T3AoOHR_ZZIqL.eM9Y.lbvANgewQGYkd3Sjzs_EkVzg',
 loginUrl = 'https://login.salesforce.com/',
 redirectURI = 'https://test09152015.herokuapp.com/oauthcallback.html',
-proxyURL = 'http://localhost:5000/proxy/',
+proxyURL = 'http://test09152015.herokuapp.com/proxy/',
 client = new forcetk.Client(clientId, loginUrl, proxyURL);
 
 function login() {
@@ -18,7 +18,7 @@ function oauthCallback(response) {
                                apiVersion,
                                response.instance_url);
         console.log('OAuth authentication succeeded');
-        getSessions();
+        //etSessions();
     } else {
         alert("AuthenticationError: No Token");
     }
